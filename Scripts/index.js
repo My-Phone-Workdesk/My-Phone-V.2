27948325ee3be5c1e9a3ab60ec2644038375c071
Ayush <ayushujjwal@yahoo.com>
2023-08-04 18:29:30 +0530

    done

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,9 +11,9 @@ function scroll() {
     document.documentElement.scrollTop = 0; //For other browsers like FIrefox, Chrome, Edge, etc...
 }
 
-function Restart() {
+function Restart(restart_time) {
     document.body.style.backgroundColor = "#000000";
-    setTimeout ( function restart_time_delay(restart_time) {
+    setTimeout ( function restart_time_delay() {
         document.body.style.backgroundColor = "#ffffff";
     }, restart_time)
 }
