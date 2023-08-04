c9b0e2a5239b546d922bfd079cae69e27deff450
Ayush <ayushujjwal@yahoo.com>
2023-08-04 18:09:27 +0530

    Restart

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,20 +4,16 @@ function eventplay(time_delay) {
     setTimeout ( function alert_after_scroll() {
         alert("This is Under Operation so don't touch it Please");
     }, time_delay);
-
-    /*
-    Set Timeout function is inbuilt in javascript and uses to delay time in this format below -->
-
-    setTimeout ( function ~Name of Function~ {
-        ~Code should be run after time delay or after this set timeout function executed~
-    }, ~Number of Seconds of Time delay x1000 to make it miliseconds~);
-
-    Set Timeout function only reads miliseconds so 1 second = 1000 miliseconds so it is said to multiply seconds of time delay by 1000
-
-    */
 }
 
 function scroll() {
     document.body.scrollTop = 0; //For Safari Browser if so...
     document.documentElement.scrollTop = 0; //For other browsers like FIrefox, Chrome, Edge, etc...
+}
+
+function Restart() {
+    document.body.style.backgroundColor = "#000000";
+    setTimeout ( function restart_time_delay(restart_time) {
+        document.body.style.backgroundColor = "#ffffff";
+    }, restart_time)
 }
