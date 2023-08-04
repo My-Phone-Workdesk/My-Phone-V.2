dae9d2dfc433a225524866c082236cf4b2128628
Ayush <ayushujjwal@yahoo.com>
2023-08-04 21:36:39 +0530

    lala

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,10 +12,13 @@ function scroll() {
 }
 
 function Restart(restart_time) {
+    var Load_Line_Elm = document.getElementById("Load_Line");
+    Load_Line_Elm.style.visibility = "hidden";
     document.body.style.backgroundColor = "#000000";
     setTimeout ( function restart_time_delay() {
         document.body.style.backgroundImage = "url('../Images/Start_Up_Logo.jpg')";
         document.body.style.backgroundSize = "100vw 90vh";
         document.body.style.backgroundRepeat = "no-repeat";
+        Load_Line_Elm.style.visibility = "visible";
     }, restart_time)
 }
