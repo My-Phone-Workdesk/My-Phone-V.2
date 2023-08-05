22d832c4c6d57bfee478217eb0e0a02424fb8b06
Ayush <ayushujjwal@yahoo.com>
2023-08-05 19:45:36 +0530

    import package

--- Scripts/index.js
+++ Scripts/index.js
@@ -30,4 +30,19 @@ function power_off() {
     document.body.style.backgroundImage = "none";
     document.body.style.background = "none";
     document.body.style.backgroundColor = "#000000";
+    var package = prompt("Add up a Package to go furthur...", "my-phone.V-2.package.OS-System.*");
+    if (package.toLowerCase() == "my-phone.v-2.package.os-system.*") {
+        console.log("Package --> My Phone V.2");
+        console.log("------------------------");
+        console.log("OS System package imported");
+        console.log("------------------------");
+        console.log("Continue Furthur Progress...");
+        console.log("------------- Go Ahead ---------");
+    } else {
+        console.log("Unknown Package imported");
+        console.log("No File Location Found on Server");
+        console.log("====== Acion Failed ============");
+        alert("Package not Found... Go Back Instead...");
+        location.href = "../index.html";
+    }
 }
