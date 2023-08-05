043c7a796b52d78f10690e2fde8dcfb79044068f
Ayush <ayushujjwal@yahoo.com>
2023-08-05 16:48:49 +0530

    poty

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,11 +11,11 @@ function scroll() {
     document.documentElement.scrollTop = 0; //For other browsers like FIrefox, Chrome, Edge, etc...
 }
 
-function Restart(restart_time) {
+function Restart() {
     document.body.style.backgroundColor = "#000000";
     setTimeout ( function restart_time_delay() {
         document.body.style.backgroundImage = "url('../Images/Start_Up_Logo.jpg')";
         document.body.style.backgroundSize = "100vw 90vh";
         document.body.style.backgroundRepeat = "no-repeat";
-    }, restart_time)
+    }, 5000)
 }
