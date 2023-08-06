8b201345cbd0651ed3a6c748098ad2650f24bf53
Ayush <ayushujjwal@yahoo.com>
2023-08-06 18:18:50 +0530

    Mother_Board

--- Scripts/Devices.js
+++ Scripts/Devices.js
@@ -1,5 +1,5 @@
 function Devices(Device) {
-    location.href = "../Screen/User_Setup/Mother_Board.html";
+    location.href = "../../Screen/User_Setup/Mother_Board.html";
     if (Device == "Desktop") {
         Mother_Board(4000, Device);
     }
@@ -8,4 +8,17 @@ function Devices(Device) {
 function Mother_Board(Money, Device) {
     let price = document.getElementById("Price");
     price.innerHTML = "Your Device Platform is : " + Device + '\n' + "Your Mother Board Cost is : " + Money;
+}
+
+function show_hide() {
+    let eye = document.getElementById("show/hide");
+    let input = document.getElementById("input_code");
+    var check = eye.className;
+    if (check.includes("slash") == true) {
+        eye.className = "fa-solid fa-eye";
+        input.type = "password";
+    } else {
+        eye.className = "fa-solid fa-eye-slash";
+        input.type = "text";
+    }
 }
