4c0bd6972536961386f1b2bd6e22df201bb95613
Ayush <ayushujjwal@yahoo.com>
2023-08-06 19:23:56 +0530

    Mother Board Done

--- Scripts/Devices.js
+++ Scripts/Devices.js
@@ -3,17 +3,21 @@ function Devices(Device) {
     if (Device == "Desktop") {
         Mother_Board(4000, Device);
     }
+    let input = document.getElementById("input_code");
+    input.focus();
 }
 
 function Mother_Board(Money, Device) {
-    let price = document.querySelector('p');
-    var statement = "Your Device Platform is : " + Device + '\n' + "Your Mother Board Cost is : " + Money;
-    price.innerHTML = statement;
+    let device = document.querySelector('p');
+    let price = document.querySelector('span');
+    device.style.bottom = "-5rem";
+    device.innerHTML = "Your Device Platform is : " + Device;
+    price.innerHTML = "Your Mother Board Cost is : " + Money;
 }
 
 function show_hide() {
-    let eye = document.getElementById("show/hide");
     let input = document.getElementById("input_code");
+    let eye = document.getElementById("show/hide");
     var check = eye.className;
     if (check.includes("slash") == true) {
         eye.className = "fa-solid fa-eye";
