cc88603f1641b03616315eec2322d75d11c5ae04
Ayush <ayushujjwal@yahoo.com>
2023-08-09 20:12:32 +0530

    Not Solved yet

--- Scripts/Devices.js
+++ Scripts/Devices.js
@@ -11,58 +11,38 @@ function show_hide() {
     }
 }
 
-document.getElementById("Desktop").addEventListener("click", () => {
-    Setup("Desktop");
-});
-document.getElementById("Laptop").addEventListener("click", () => {
-    Setup("Laptop");
-});
-document.getElementById("Notepad").addEventListener("click", () => {
-    Setup("Notepad");
-});
-document.getElementById("Foldable").addEventListener("click", () => {
-    Setup("Foldable");
-});
-document.getElementById("Tablet").addEventListener("click", () => {
-    Setup("Tablet");
-});
-document.getElementById("Phone").addEventListener("click", () => {
-    Setup("Phone");
-});
-
 var device_type = null;
 var amount_MB = 0;
 
-function Setup(name) {
-    switch (name) {
+function Setup() {
+    switch (window.device_type) {
         case "Desktop":
-            amount_MB = 4000;
+            window.amount_MB = 4000;
             break;
         case "Laptop" :
-            amount_MB = 2000;
+            window.amount_MB = 2000;
             break;
         case "Notepad" :
-            amount_MB = 1500;
+            window.amount_MB = 1500;
             break;
         case "Foldable" :
-            amount_MB = 1000;
+            window.amount_MB = 1000;
             break;
         case "Tablet" :
-            amount_MB = 800;
+            window.amount_MB = 800;
             break;
         case "Phone" :
-            amount_MB = 500;
+            window.amount_MB = 500;
             break;
         default :
-            name = "Administrative Device";
-            amount_MB = 5000;
+            window.device_type = "Administrative Device";
+            window.amount_MB = 5000;
             break;
     }
-    device_type = name;
     location.href = "../../Screen/User_Setup/Mother_Board.html";
 }
 
 function Mother_Board() {
-    document.querySelector('p').innerHTML = "Your Selected Device is : " + device_type;
-    document.querySelector('span').innerHTML = "Your Mother Board Price Bill is : " + amount_MB;
+    document.querySelector('p').innerHTML = "Your Selected Device is : " + window.device_type;
+    document.querySelector('span').innerHTML = "Your Mother Board Price Bill is : " + window.amount_MB;
 }
