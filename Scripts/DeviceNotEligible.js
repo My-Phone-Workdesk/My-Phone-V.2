620527239c0af68606be1fbb32f8689eae1af08e
LALA Sengar <100473616+LalaLolLucky@users.noreply.github.com>
2023-08-11 21:43:47 +0530

    Create DeviceNotEligible.js

---
+++ Scripts/DeviceNotEligible.js
@@ -0,0 +1,6 @@
+const minwidth = window.matchMedia("(min-width: 0px)");
+const maxwidth = window.matchMedia("(max-width: 1279px)");
+
+if (!(minwidth.matches && maxwidth.matches)) {
+  location.href="../DeviceNotEligible.html";
+}
