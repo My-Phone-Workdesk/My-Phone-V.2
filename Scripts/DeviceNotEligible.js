f2f3103cef712e66591e704e25d447e3a5537eb5
LALA Sengar <100473616+LalaLolLucky@users.noreply.github.com>
2023-08-11 21:45:42 +0530

    Update DeviceNotEligible.js

--- Scripts/DeviceNotEligible.js
+++ Scripts/DeviceNotEligible.js
@@ -1,6 +1,7 @@
-const minwidth = window.matchMedia("(min-width: 0px)");
-const maxwidth = window.matchMedia("(max-width: 1279px)");
-
-if (!(minwidth.matches && maxwidth.matches)) {
-  location.href="../DeviceNotEligible.html";
+function runOnStart() {
+  const minwidth = window.matchMedia("(min-width: 0px)");
+  const maxwidth = window.matchMedia("(max-width: 1279px)");
+  if (!(minwidth.matches && maxwidth.matches)) {
+    location.href="../DeviceNotEligible.html";
+  }
 }
