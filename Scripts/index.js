6834e268505b3589eb7626e306fa0a4097aad7b6
LALA Sengar <100473616+LalaLolLucky@users.noreply.github.com>
2023-08-12 12:31:39 +0530

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -46,6 +46,6 @@ function runOnStart() {
     const minwidth = window.matchMedia("(min-width: 0px)");
     const maxwidth = window.matchMedia("(max-width: 1279px)");
     if (minwidth.matches && maxwidth.matches) {
-      location.href="./../DeviceNotEligible.html";
+      location.href="DeviceNotEligible.html";
     }
   }
