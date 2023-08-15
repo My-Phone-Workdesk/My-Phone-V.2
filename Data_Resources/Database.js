dd4b7a02c5cf9a968083dd66146f476b0986c060
Lala Coder <ayushujjwal@yahoo.com>
2023-08-15 13:44:51 +0530

    modified: Data_Resources/Database.js

--- Data_Resources/Database.js
+++ Data_Resources/Database.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+/*
 fs.readFile('../Data_Resources/Database.txt', 'utf-8', (err, data) => {
     if (err) {
         console.error(err);
@@ -8,11 +9,33 @@ fs.readFile('../Data_Resources/Database.txt', 'utf-8', (err, data) => {
     console.log(data);
 });
 
-const content = "Github.com";
+*/
 
-fs.writeFile('Database.txt', content, err => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-});
+const name = "Lala";
+
+const content = "My Name is " + name;
+
+for (var x = 0; x <= 10; x++) {
+    fs.appendFile('Data_Resources/Database.txt', '\n' + content, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+}
+
+/*
+
+const request = window.indexedDB.open("Database.txt");
+
+request.onerror( function () {
+    //Error...
+    console.error("I have many Errors within me...");
+} );
+
+request.onsuccess( function () {
+    //Yes, I did it...
+    console.warn("You are getting good in JS and its Database...");
+} );
+
+*/
