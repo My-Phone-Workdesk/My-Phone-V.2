5725fb336a77890a91a19d45e73052b0d0ce445d
Lala Coder <ayushujjwal@yahoo.com>
2023-08-15 15:51:07 +0530

    Chodo

--- Data_Resources/Database.js
+++ Data_Resources/Database.js
@@ -1,41 +1,10 @@
 const fs = require('fs');
 
-/*
-fs.readFile('../Data_Resources/Database.txt', 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
-
-*/
-
-const name = "Lala";
-
-const content = "My Name is " + name;
-
-for (var x = 0; x <= 10; x++) {
-    fs.appendFile('Data_Resources/Database.txt', '\n' + content, err => {
+function AddData() {
+    fs.appendFile('Data_Resources/Database.txt', '\n' + "Empty", err => {
         if (err) {
             console.error(err);
             return;
         }
     });
 }
-
-/*
-
-const request = window.indexedDB.open("Database.txt");
-
-request.onerror( function () {
-    //Error...
-    console.error("I have many Errors within me...");
-} );
-
-request.onsuccess( function () {
-    //Yes, I did it...
-    console.warn("You are getting good in JS and its Database...");
-} );
-
-*/
