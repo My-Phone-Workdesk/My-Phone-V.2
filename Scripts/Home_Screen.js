b1365a7dd347a3f596ef8c52c70001bdb458d512
Lala Coder <ayushujjwal@yahoo.com>
2023-08-21 19:38:57 +0530

    CMD

---
+++ Scripts/Home_Screen.js
@@ -0,0 +1,18 @@
+function Load_Home_Screen() {
+    //Play Store ( System App ) -->
+    const play_store = document.body.appendChild( document.createElement('i') );
+    play_store.className = "fa-brands fa-google-play";
+    play_store.title = "Play Store -- System App";
+    //Settings ( System App ) -->
+    const settings = document.body.appendChild( document.createElement('i') );
+    settings.className = "fa-solid fa-gear";
+    settings.title = "Settings -- System App";
+    //CMD ( System App for Laptop, Desktop and Notepad ) ==>
+    const CMD = document.body.appendChild( document.createElement('span') );
+    CMD.style.backgroundImage = "url('../Icons/app.system.CMD.ico')";
+    CMD.title = "CMD -- Command Prompt";
+    //Clicking on App Icons -->
+    CMD.addEventListener('click', function () {
+        location.href = "./Apps/CMD.html";
+    });
+}
