84fe260150fa3abc9a0c52b56afdbf4b630f46dc
Lala Coder <ayushujjwal@yahoo.com>
2023-08-25 21:50:18 +0530

    Printing Press is done through CMD

--- Scripts/App/CMD.js
+++ Scripts/App/CMD.js
@@ -23,7 +23,8 @@ function Scripting() {
     var code_array = new Array();
     code_array = JSON.parse( localStorage.getItem("Code") );
     var field = "normal";
-    for (var line = 0; line <= code_array.length; line++) {
+    var print = "nothing";
+    for (var line = 0; line < code_array.length; line++) {
         field = check_Syntax_Array( code_array[line], line);
         document.write(field);
         if ( field.includes("Error") ) {
@@ -32,7 +33,15 @@ function Scripting() {
         } else {
             document.write("<br>");
         }
-    }
+    }   //Here the Compilation Finishes...
+    setTimeout( () => {
+        document.write("<br>");
+        document.write( "-".repeat(45) + " Output " + "-".repeat(45) );
+        document.write("<br>");
+        for (var line = 0; line < code_array.length; line++) {
+            Run_Code( code_array[line] );
+        }
+    },2000 );
 }
 
 function check_Syntax_Array(Syntax, line) {
@@ -43,11 +52,11 @@ function check_Syntax_Array(Syntax, line) {
     } // Checking of the Symbol ';' at the end of the Line or Syntax...
     if ( Syntax.includes("Print") ) {
         if ( Syntax.charAt(Syntax.length - 1) == ";" ) {
-            if ( ( Syntax.slice(-4, -1) ) == ")))" && ( Syntax.slice(5, 7) ) == "(((") {
+            if ( ( Syntax.slice(-4, -1) ) == ")))" && ( Syntax.slice(5, 8) ) == "(((") {
                 // No Error in Printing String...
-            } else if ( ( Syntax.slice(-3, -1) ) == "))" && ( Syntax.slice(6, 7) ) == "))") {
+            } else if ( ( Syntax.slice(-3, -1) ) == "))" && ( Syntax.slice(5, 7) ) == "((") {
                 // No Error in Printing Variables...
-            } else if ( ( Syntax.slice(-1) ) == ")" && ( Syntax.slice(7) ) == ")") {
+            } else if ( ( Syntax.slice(-2, -1) ) == ")" && ( Syntax.slice(5, 6) ) == "(") {
                 // No Error in Printing Integer Value...
             } else {
                 var error = " Print() function must include atleast one character ')' in start and before last... "
@@ -58,5 +67,25 @@ function check_Syntax_Array(Syntax, line) {
             return "Error in line : " + line + " : " + error;
         }
     }
+    if ( Syntax.charAt(0) == '#' ) {
+        //Its a Comment just compile or ignore it...
+        return "Commentary Line..." + "Line == " + line;
+    }
     return "Syntax of Line : " + line + " : Successfully Compiled...";
+}
+
+function Run_Code(statement) {
+
+    if ( statement.includes("Print") ) {
+        if ( statement.includes("(((") ) {
+            document.write( statement.slice(8, -4) );
+            document.write("<br>");
+        } else if ( statement.includes("((") ) {
+            //variable Value...
+        } else if ( statement.includes("(") ) {
+            document.write( statement.slice(6, -2) );
+            document.write("<br>");
+        }
+    }
+    
 }
