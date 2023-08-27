dba8320d147fa5182986b5ccf5c459c30648b0ab
LALA Sengar <100473616+LalaLolLucky@users.noreply.github.com>
2023-08-27 14:53:50 +0000

    Faltu

--- Data_Resources/Database.php
+++ Data_Resources/Database.php
@@ -1,4 +1,34 @@
 <?php
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+# [START cloud_sql_mysql_pdo_connect_tcp]
+namespace Google\Cloud\Samples\CloudSQL\MySQL;
+
+use PDO;
+use PDOException;
+use RuntimeException;
+use TypeError;
+
+class DatabaseTcp
+{
+    public static function initTcpDatabaseConnection(): PDO
+    {
         try {
             // Note: Saving credentials in environment variables is convenient, but not
             // secure - consider a more secure solution such as
@@ -9,26 +39,6 @@
             $dbName = getenv('DB_NAME'); // e.g. 'your_db_name'
             $instanceHost = getenv('INSTANCE_HOST'); // e.g. '127.0.0.1' ('172.17.0.1' for GAE Flex)
 
-<<<<<<< HEAD
-define('HOST', 'asia-south2.c8413d9b-cba7-4c08-b7f2-7c0e6b294870.gcp.ybdb.io');
-define('PORT', '5433');
-define('DB_NAME', 'yugabyte');
-define('USER', 'lala9211');
-define('PASSWORD', 'Lala9211');
-define('SSL_MODE', 'verify-full');
-define('SSL_ROOT_CERT', './root.crt');
-
-$conn_str = 'postgresql://' . DB_NAME . ':' . PASSWORD . '@' . HOST . ':' . PORT . '/' . DB_NAME . '?ssl=true&sslmode=verify-full&sslrootcert=' .  SSL_ROOT_CERT ;
-
-try {
-    $conn = new PDO($conn_str);
-    echo "Connect to database sucessful";
-} catch (PDOException $e) {
-    echo "fail to connect to database" . $e->getMessage();
-}
-
-?>
-=======
             // Connect using TCP
             $dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $instanceHost);
 
@@ -73,4 +83,8 @@ try {
                 $e
             );
         }
->>>>>>> b1498e2 (Faltu)
+
+        return $conn;
+    }
+}
+# [END cloud_sql_mysql_pdo_connect_tcp]
