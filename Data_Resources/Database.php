5e528a0077e0d0484c5abe5e0d5a67da067d82b4
LALA Sengar <100473616+LalaLolLucky@users.noreply.github.com>
2023-08-29 10:39:15 +0000

    gk,k

--- Data_Resources/Database.php
+++ Data_Resources/Database.php
@@ -1,33 +1,17 @@
 <?php
-/*
- * Copyright 2020 Google LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 declare(strict_types=1);
 
 # [START cloud_sql_mysql_pdo_connect_tcp]
-namespace Google\Cloud\Samples\CloudSQL\MySQL;
+namespace My_Phone_V2\Data_Resources;
 
 use PDO;
 use PDOException;
 use RuntimeException;
 use TypeError;
 
-class DatabaseTcp
+class Database
 {
-    public static function initTcpDatabaseConnection(): PDO
+    public static function initDatabaseConnection(): PDO
     {
         try {
             // Note: Saving credentials in environment variables is convenient, but not
