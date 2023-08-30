42c9988473acfeff33bb1908b52470e4e41b73e7
Lala Coder <ayushujjwal@yahoo.com>
2023-08-30 17:12:32 +0530

    Correct Spaces in Password.com.package.input.in

--- Screen/User_Setup/Create_User_Profile.js
+++ Screen/User_Setup/Create_User_Profile.js
@@ -13,7 +13,13 @@ function Create_User_Profile() {
 }
 
 function Submit_Details() {
-
+    let Check_Box = document.getElementById("Password_Strength");
+    let Color = Check_Box.style.backgroundColor;
+    if ( Color == 'red' ) {
+        alert("Password Must not contain Spaces and must be of or more than 8 characters...");
+    } else {
+        
+    }
 }
 
 function Check_Password_Strength() {
@@ -21,16 +27,20 @@ function Check_Password_Strength() {
     let password_input = document.getElementById("user_password");
     let password = password_input.value;
     var Strong_Char = new Array();
-    Strong_Char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '`', '~', ',', '.', '<', '>', '/', '?', ':', ';', '[', ']', '{', '}', '|', '"', "'"];
-    if ( password.length >= 8 ) {
-        Check_Box.style.backgroundColor = 'yellow';
-        for (var no = 0; no < Strong_Char.length; no++) {
-            if (password.includes( Strong_Char[no] ) ) {
-                Check_Box.style.backgroundColor = 'green';
-                return "Strong";
-            }
-        }   
-    } else {
+    Strong_Char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '`', '~', ',', '.', '<', '>', '/', '?', ':', ';', '[', ']', '{', '}', '|', '"', "'", '_'];
+    if ( password.includes(' ') ) {
         Check_Box.style.backgroundColor = 'red';
+    } else {
+        if ( password.length >= 8 ) {
+            Check_Box.style.backgroundColor = 'yellow';
+            for (var no = 0; no < Strong_Char.length; no++) {
+                if (password.includes( Strong_Char[no] ) ) {
+                    Check_Box.style.backgroundColor = 'green';
+                    return "Strong";
+                }
+            }   
+        } else {
+            Check_Box.style.backgroundColor = 'red';
+        }
     }
 }
