c8af6af93b95a168790ff79908b9525bfafb7728
Lala Coder <ayushujjwal@yahoo.com>
2023-09-02 15:40:42 +0530

    Database using Sheets Theory

--- Data_Resources/Database.js
+++ Data_Resources/Database.js
@@ -39,8 +39,53 @@ function Common_Method(process) {
     return process;
 }
 
+/* वेसे तो ये काम नहीं करता पर फिर भी क्या पता काम आ जाए ॥
 async function Get_Data() {
     let n = await fetch('https://script.google.com/macros/s/AKfycbykj7egp6Z2N150ZBGs6NlwssPyw6-Cs5XjRqPZeBgeDwMCUdshtU10ewA9KFRpU3uDJA/exec');
     let resp = await n.json();
     console.log(resp, n);
 }
+
+Common API KEY == https://sheetdb.io/api/v1/qhlszwbu7dxp7
+Just add Sheet name after the API...
+To Select a range add after sheet name += &limit=(Number of Rows)
+To pass the range between two rows add after limit += &offset= (After which row means row number...)
+For Example ***&limit=10&offset=10 so it will select 10 rows after row 10 means 11 to 20...
+
+But if you want to select values under a column header for example you want to select all the names that are available under name column so add this after comman API key += /Keys
+
+But if you want to get all the names of Columns add after Common API Key += /keys
+
+You can also add Sheet name after /keys by putting ?sheet=(name of the sheet)...
+
+To get the sheet name just add up after commom API Key += /name...
+
+To get all the sheets name on the workbook just add after common API key += /sheets...
+
+To count how many rows are present in the sheet just add up after common API key += /count
+and you can also add ?sheet=(Sheet Name)... after /count to refer to a sheet...
+
+To put a condition according to column name put this after sheet name &(Column name)=(Column Value)
+and you can put so on using & operator in API KEY... But in this case put /search after common API KEY... so for example --> https://sheetdb.io/api/v1/qhlszwbu7dxp7/search?sheet=User_Accounts&User=Owner
+
+You can also put /search_or to check that any one condition is true...
+You can also put &limit=(limit) after search... that will give you the first searched data...
+If you put only search that will work like search_and but this doesn't exist so only put search...
+
+There is also a parameter that is &casesensitive=(true/false) which check the case sensivity of the data that is that true or false... accordingly to the given coditions...
+
+Sorting Data ==> &sort_by=(name of the Column)
+You can also change sort type by &sort_order = (desc/asc)
+
+There is another parameter too that is &single_object=(true/false)
+The above will give the first record in the form of an object not in JSON...
+
+
+To Update an record just add up '/' + (Column Name) + '/' + (Row value) after the Common API key
+You can also put ?sheet= (sheet name) to update data in a specific sheet...
+Update will work in PUT request...
+
+To add data you have to change the request to POST and just add common api key and then select the sheet in which you want to add data by using ?sheet= (sheet name) and fill the values in a JSON file of what to add the data and in which field...
+
+To delete Data records you need to change the request to DELETE and the format would be same as Update one but here it will delete the whole row instead of updating the row's value...*/
+
