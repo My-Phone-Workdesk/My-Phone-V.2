b807fa75f8aacc46cdc2a28178986c6b2f0734db
Lala Coder <ayushujjwal@yahoo.com>
2023-09-09 16:38:17 +0530

    Files Data Startup Done

--- Scripts/System/System.js
+++ Scripts/System/System.js
@@ -1,3 +1,20 @@
+// Exported Functions From Database ==>
+
+const request = new XMLHttpRequest();
+const Database = 'https://sheetdb.io/api/v1/qhlszwbu7dxp7';
+
+function Database_UpdateData(sheet, argument, record) {
+    var Database_URL = Database + argument + sheet;
+    request.open("PUT", Database_URL);
+    request.setRequestHeader('Content-type','application/json; charset=utf-8');
+    request.onload = () => {
+        console.log(request.status);
+        console.clear();
+    }; request.send(record);
+}
+
+// Real Script Starts from Below ==>
+
 function Restart() {
 
     document.body.style.backgroundColor = "#000000";
@@ -17,5 +34,35 @@ function Restart() {
         setTimeout( () => { location.href = "../../index.html"; },2000 );
            
     }, 15575);
+
+}
+
+function Files( value ) {
+
+    var new_value = '';
+
+    for ( var v = 0; v <= value.length; v++ ) {
+
+        if ( value.charAt(v) == '"' ) { new_value += "'"; }
+        else if ( value.charAt(v) == "'" ) { new_value += '"' }
+        else { new_value += value.charAt(v) }
+
+    }; return new_value;
+
+}
+
+function Upload_Storage( data, user ) {
+
+    data = JSON.stringify( data );
+
+    data = Files( data );
+
+    data = { "Data": data };
+
+    data = JSON.stringify( data );
+
+    Database_UpdateData("?sheet=Files", "/User/" + user, data);
+
+    setTimeout( () => {},2000 );
 
 }
