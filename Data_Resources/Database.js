e75c2c368ad5313926ef00b4c949825d08367430
Lala Coder <ayushujjwal@yahoo.com>
2023-09-11 19:28:14 +0530

    Database Origin Updated

--- Data_Resources/Database.js
+++ Data_Resources/Database.js
@@ -116,3 +116,69 @@ Update will work in PUT request...
 To add data you have to change the request to POST and just add common api key and then select the sheet in which you want to add data by using ?sheet= (sheet name) and fill the values in a JSON file of what to add the data and in which field...
 
 To delete Data records you need to change the request to DELETE and the format would be same as Update one but here it will delete the whole row instead of updating the row's value... */
+
+const CLIENT_ID = '463724458932-eeagdhubnlq301h6656v042vk4t645c2.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAXdwGN4T6QDFVz6aIC4YnKY-iVvUttqRM';
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+function gapiLoaded() { gapi.load('client', initializeGapiClient); }
+
+async function initializeGapiClient() {
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true; }
+
+function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '',
+    });
+    gisInited = true; }
+
+var sheet_name = 'User_Accounts';
+
+async function listData() { let response;
+    try {
+        response = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1-jMb9tOG--iC9_onIXqK1LTWKGqs0H7iOTXDTu7W1gs',
+        range: sheet_name,
+        });
+    } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('content').innerText = 'No values found.';
+        return;
+    }
+    const output = range.values.reduce((str, row) => `${str}\n${row}`);
+    document.getElementById('content').innerText = output; 
+    var result = new Array(); result = output.split('\n');
+    var Headers = new Array(); Headers = result[0].split(',');
+    var Final_Data = new Array();
+    Single_User_Data = new Array(); All_User_Data = new Array();
+    for ( var v = 1; v < result.length; v++ ) {
+        All_User_Data.push( result[v].split(',') );
+    }; for ( var a = 0; a < All_User_Data.length; a++ ) {
+        Single_User_Data = All_User_Data[a];
+        if ( Single_User_Data.length == Headers.length ) {
+            var classify = new Object();
+            for ( var s = 0; s < Headers.length; s++ ) {
+                classify[Headers[s]] = Single_User_Data[s];
+            }; Final_Data.push(classify);
+        } else {
+            console.log("Error : Missing Details...");
+        }
+    }; Final_Data = JSON.stringify(Final_Data); sessionStorage.setItem('Data', Final_Data); }
+
+async function updateData() {
+    alert("I do not work at all!");
+}
