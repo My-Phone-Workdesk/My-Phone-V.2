80768a6dcd92d27d67fb02e59944c81abd90a822
Lala Coder <ayushujjwal@yahoo.com>
2023-09-29 15:47:00 +0530

    Improving Payment Database

--- Scripts/Devices.js
+++ Scripts/Devices.js
@@ -1,91 +1,187 @@
 // Exported Functions From Database ==>
+
+import { Database } from '../Data_Resources/Database.js';
+
 // Real Script Starts from Below ==>
 
+window.onload = () => {
+
+    if ( location.pathname.includes( 'Mother_Board.html' ) ) {
+
+        Mother_Board();
+
+        let show_or_hide = document.getElementById( 'show/hide' );
+        let Payment = document.getElementById( 'Payment' );
+        let skip = document.getElementById( 'Skip' );
+
+        show_or_hide.addEventListener( 'click', show_hide );
+        Payment.addEventListener( 'click', Payment_MB );
+        skip.addEventListener( 'click', Skip );
+
+    } else if ( location.pathname.includes( 'Devices.html' ) ) {
+
+        let Desktop = document.getElementById( 'Desktop' );
+        let Laptop = document.getElementById( 'Laptop' );
+        let Notepad = document.getElementById( 'Notepad' );
+        let Foldable = document.getElementById( 'Foldable' );
+        let Tablet = document.getElementById( 'Tablet' );
+        let Phone = document.getElementById( 'Phone' );
+
+        Desktop.addEventListener( 'click', () => {
+
+            localStorage.setItem( 'device_type', 'Desktop' ); Setup();
+
+        });
+
+        Laptop.addEventListener( 'click', () => {
+
+            localStorage.setItem( 'device_type', 'Laptop' ); Setup();
+
+        });
+
+        Notepad.addEventListener( 'click', () => {
+
+            localStorage.setItem( 'device_type', 'Notepad' ); Setup();
+
+        });
+
+        Foldable.addEventListener( 'click', () => {
+
+            localStorage.setItem( 'device_type', 'Foldable' ); Setup();
+
+        });
+
+        Tablet.addEventListener( 'click', () => {
+
+            localStorage.setItem( 'device_type', 'Tablet' ); Setup();
+
+        });
+
+        Phone.addEventListener( 'click', () => {
+
+            localStorage.setItem( 'device_type', 'Phone' ); Setup();
+
+        });
+
+    };
+
+};
+
 function show_hide() {
+
     let input = document.getElementById("input_code");
     let eye = document.getElementById("show/hide");
+
     var check = eye.className;
+
     if (check.includes("slash") == true) {
+
         eye.className = "fa-solid fa-eye";
         input.type = "password";
+
     } else {
+
         eye.className = "fa-solid fa-eye-slash";
         input.type = "text";
-    }
-}
+
+    };
+
+};
 
 function Setup() {
+
     var know = localStorage.getItem("device_type");
+
     switch (know) {
+
         case "Desktop":
-            localStorage.setItem("Amount_MB", 4000);
-            break;
+
+            localStorage.setItem("Amount_MB", 4000); break;
+
         case "Laptop":
-            localStorage.setItem("Amount_MB", 2000);
-            break;
+
+            localStorage.setItem("Amount_MB", 2000); break;
+
         case "Notepad":
-            localStorage.setItem("Amount_MB", 1500);
-            break;
+
+            localStorage.setItem("Amount_MB", 1500); break;
+
         case "Foldable":
-            localStorage.setItem("Amount_MB", 1000);
-            break;
+
+            localStorage.setItem("Amount_MB", 1000); break;
+
         case "Tablet":
-            localStorage.setItem("Amount_MB", 800);
-            break;
+
+            localStorage.setItem("Amount_MB", 800); break;
+
         case "Phone":
-            localStorage.setItem("Amount_MB", 500);
-            break;
+
+            localStorage.setItem("Amount_MB", 500); break;
+
         default:
+
             localStorage.setItem("device_type", "Administrative Device");
-            localStorage.setItem("Amount_MB", 5000);
-            break;
-    }
-    window.location.href = "../../Screen/User_Setup/Mother_Board.html";
-}
+            localStorage.setItem("Amount_MB", 5000); break;
+
+    }; window.location.href = "../../Screen/User_Setup/Mother_Board.html";
+
+};
 
 function Mother_Board() {
+
     var does = localStorage.getItem("device_type");
     var did = localStorage.getItem("Amount_MB");
+
     document.querySelector('p').innerHTML = "Selected Device: " + does;
     document.querySelector('span').innerHTML = "Price of Mother Board: " + did;
-}
+
+};
 
 function Payment_MB() {
+
     try {
+
         var a = new Array();
         a = JSON.parse(sessionStorage.getItem("Accounts_Data"));
         var Data_list = new Array();
+
         for (var b = 0; b < a.length; b++) {
+
             var c = a[b];
             Data_list.push( c["Security_Code"] );
-        }
-        a = Data_list;
+
+        }; a = Data_list;
         Data_list = null;
+
         var b = document.getElementById('input_code').value;
         var d = a.length;
 
         for (var c = 0; c < d; c = c ) {
+
                 if (a[c] == b) {
+
                     if (c > 0) {
+
                         a = JSON.parse(sessionStorage.getItem("Accounts_Data") );
                         Data_list = new Array();
+
                         for (var ab = 0; ab < a.length; ab++) {
+
                             var ac = a[ab];
                             Data_list.push( ac["Money"] );
-                        }
-                        a = Data_list;
+
+                        }; a = Data_list;
                         Data_list = null;
+
                         d = localStorage.getItem("Amount_MB");
                         d = parseFloat(d);
                         a[c] = parseFloat( a[c] );
+
                         if ( a[c] >= d ) {
-                            a[c] -= d;
-                            d = new Object();
-                            d.Money = a[c];
-                            d = JSON.stringify(d);
-                            a = null;
+
+                            a[c] -= d; d = a[c]; a = null;
                             
-                            // Database_UpdateData("?sheet=Accounts", '/ID/' + c, d);
+                            Database.Update_Data( 'Accounts', 'J' + ( c + 2 ), d );
 
                             document.body.style.cursor = "Progress";
 
@@ -97,12 +193,11 @@ function Payment_MB() {
                                 localStorage.setItem("Add_User", Account);
 
                                 var update = JSON.parse( sessionStorage.getItem("Accounts_Data") );
-                                (update[c]).Money = ( JSON.parse(d) ).Money;
+                                (update[c]).Money = d; d = null;
                                 update = JSON.stringify( update );
                                 sessionStorage.setItem("Accounts_Data", update);
 
                                 alert("Payment Successful");
-                                location.reload();
                                 location.href = "../../OS_Package/OS_Setup/OS.html";
 
                                 document.body.style.cursor = "Default";
@@ -110,25 +205,26 @@ function Payment_MB() {
                             },2000 ); return 1;
                             
                         } else {
+
                             alert("Sorry, You can't Proceed ahead due to insufficient Account Balance");
-                            location.reload();
-                            return -2;
-                        }
+                            location.reload(); return -2;
+
+                        };
+
                     } else if (c == 0) {
+
                         alert("You can't use Government Financial Money...");
-                        location.reload();
-                        return 0;
-                    }
-                } else {
-                    c++;
-                }
+                        location.reload(); return 0;
+
+                    };
+
+                } else { c++; };
 
             }; alert("Wrong Security Code"); location.reload(); return -1;
 
-    } catch (error) {
-        alert(error.message);
-    }
-}
+    } catch (error) { alert(error.message); }
+    
+};
 
 function Skip() {
 
@@ -143,6 +239,7 @@ function Skip() {
             if ( command == "No" ) {
 
                 document.body.style.cursor = "Progress";
+
                 setTimeout( () => {
 
                     document.body.style.cursor = "Default";
@@ -164,28 +261,23 @@ function Skip() {
 
                 if ( command == '/reboot' ) {  location.href = "../../index.html"; }
 
-            } else if ( command == null ) {} else {
-
-                alert("Not an appropriate answer");
-
-            }
+            } else if ( command == null ) {} else { alert("Not an appropriate answer"); }
 
 
         } while ( command != "No" && command != '/exit' && command != null && command != '/reboot' && command != '/restart' && command != '/power off' );
 
     }; return false; // The User don't want to command to Boot...
 
-}
+};
 
 function Check_Command( command ) {
 
     if ( command == null || command == "" ) {
         
-        alert("Please Enter a Command... Can't access empty command box...");
-        return null;
+        alert("Please Enter a Command... Can't access empty command box..."); return null;
 
     } else if ( command == '/exit' || command == '/reboot' ) { sessionStorage.clear(); }
     else if ( command == "/restart" ) { location.href = "../System/Restart.html"; }
     else if ( command == '/power off' ) { location.href = "../System/Power_Off.html" }
 
-}
+};
