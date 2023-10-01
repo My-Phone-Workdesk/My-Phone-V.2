6f3a14604aadd4ef153cffb3041cead15166f6e3
Lala Coder <ayushujjwal@yahoo.com>
2023-10-01 16:04:50 +0530

    Files added to Home Screen

--- Scripts/Home_Screen.js
+++ Scripts/Home_Screen.js
@@ -1,5 +1,11 @@
 function Load_Home_Screen() {
 
+    const User_ID = localStorage.getItem( 'Amount_MB' );
+
+    var User_Data = sessionStorage.getItem( 'Data' );
+    User_Data = JSON.parse( User_Data );
+    User_Data = User_Data[ User_ID ];
+
     //Play Store ( System App ) -->
 
     const play_store = document.body.appendChild( document.createElement('i') );
@@ -14,13 +20,25 @@ function Load_Home_Screen() {
 
     //CMD ( System App for Laptop, Desktop and Notepad ) ==>
 
-    const CMD = document.body.appendChild( document.createElement('i') );
+    if ( ( User_Data[ 'Device' ] == 'Desktop' ) || ( User_Data[ 'Device' ] == 'Laptop' )
+    || ( User_Data[ 'Device' ] == 'Notepad' ) ) {
+
+        const CMD = document.body.appendChild( document.createElement('i') );
     
-    CMD.className = "fa-solid fa-rectangle-terminal";
-    CMD.title = "CMD -- Command Prompt";
+        CMD.className = "fa-solid fa-rectangle-terminal";
+        CMD.title = "CMD -- Command Prompt";
+
+        CMD.addEventListener('click', () => { location.href = "./Apps/CMD/CMD.html"; });
+
+    };
+
+    //File Explorer ( System App ) ==>
+
+    const File_Explorer = document.body.appendChild( document.createElement( 'i' ) );
+
+    File_Explorer.className = 'fa-solid fa-folder-user';
+    File_Explorer.title = 'File Explorer -- System App';
 
     //Clicking on App Icons -->
-
-    CMD.addEventListener('click', () => { location.href = "./Apps/CMD/CMD.html"; });
 
 };
