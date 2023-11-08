53d1ca3915319c7ff6611e1534840a218c0c5b30
Vikas Sengar T0213254 <vikas.sengar@thalesgroup.com>
2023-11-08 21:43:29 +0530

    Making a login page and now not with bad 'prompts'

---
+++ Scripts/Login.js
@@ -0,0 +1,55 @@
+function Login() {
+    var answer = document.getElementById('username').value;
+    if (answer != null) {
+        var list = new Array();
+        list = JSON.parse( sessionStorage.getItem("Data") );
+        var User_list = new Array();
+        for (var a = 0; a < list.length; a++) {
+            var b = list[a];
+            User_list.push( b["User"] );
+        }
+        list = User_list;
+        User_list = null;
+        for (var check = 0; check <= (list.length - 1); check++) {
+            var respond = list[check];
+            if ( respond.toLowerCase() == answer.toLowerCase() ) {
+                //The User Passed Away
+                if ( ! ( answer.toLowerCase() == "owner" ) ) {
+                    var locks_hash = new Array();
+                    locks_hash = JSON.parse( sessionStorage.getItem("Data") );
+                    User_list = new Array();
+                    for (var a = 0; a < locks_hash.length; a++) {
+                        var b = locks_hash[a];
+                        User_list.push( b["User_Lock"] );
+                    }
+                    locks_hash = User_list;
+                    User_list = null;
+                    do {
+                        var user_lock = document.getElementById('userlock').value;
+                        if ( user_lock != null ) {
+                            if ( user_lock == locks_hash[check] ) {
+                                //Correct User Lock Entered... User Recognised...!!!
+                                localStorage.setItem("Amount_MB", check);
+                                location.href = "OS_Loader.html";
+                            } else if ( user_lock.toLowerCase() == "./exit" ) {
+                                //Stop it and return
+                                return 9211;
+                            } else {
+                                alert("Incorrect User Lock Entered...");
+                            }
+                        } else {
+                            return 9211;
+                        }
+                        
+                    } while ( ! ( user_lock == locks_hash[check] ) );
+                } else {
+                    //No User Lock for the Public User Owner...
+                    localStorage.setItem("Amount_MB", 0);
+                    location.href = "OS_Loader.html";
+                }
+                return;
+            } //Do not Return Anything just add 1 to check variable
+        }
+        alert("User ID not Available");
+    }
+};
--- Scripts/index.js
+++ Scripts/index.js
@@ -12,9 +12,6 @@ window.onload = () => {
 
         Check_Data();
 
-        let Login_button = document.getElementById( 'different' );
-        Login_button.addEventListener( 'click', () => { Login(); });
-
         let Send_Feedback_button = document.getElementById( 'Send_Feed' );
         Send_Feedback_button.addEventListener( 'click', () => { Send_Feedback(); });
 
@@ -80,62 +77,6 @@ function runOnStart() {
         if (location.pathname.includes("/Device_Not_Eligible.html")) {
             location.href = "index.html";
         }
-    }
-};
-
-function Login() {
-    var answer = prompt("Login to an Existing User by its User Id", "Owner");
-    if (answer != null) {
-        var list = new Array();
-        list = JSON.parse( sessionStorage.getItem("Data") );
-        var User_list = new Array();
-        for (var a = 0; a < list.length; a++) {
-            var b = list[a];
-            User_list.push( b["User"] );
-        }
-        list = User_list;
-        User_list = null;
-        for (var check = 0; check <= (list.length - 1); check++) {
-            var respond = list[check];
-            if ( respond.toLowerCase() == answer.toLowerCase() ) {
-                //The User Passed Away
-                if ( ! ( answer.toLowerCase() == "owner" ) ) {
-                    var locks_hash = new Array();
-                    locks_hash = JSON.parse( sessionStorage.getItem("Data") );
-                    User_list = new Array();
-                    for (var a = 0; a < locks_hash.length; a++) {
-                        var b = locks_hash[a];
-                        User_list.push( b["User_Lock"] );
-                    }
-                    locks_hash = User_list;
-                    User_list = null;
-                    do {
-                        var user_lock = prompt("Enter the User Lock of the User " + answer, "");
-                        if ( user_lock != null ) {
-                            if ( user_lock == locks_hash[check] ) {
-                                //Correct User Lock Entered... User Recognised...!!!
-                                localStorage.setItem("Amount_MB", check);
-                                location.href = "Screen/OS_Loader.html";
-                            } else if ( user_lock.toLowerCase() == "./exit" ) {
-                                //Stop it and return
-                                return 9211;
-                            } else {
-                                alert("Incorrect User Lock Entered...");
-                            }
-                        } else {
-                            return 9211;
-                        }
-                        
-                    } while ( ! ( user_lock == locks_hash[check] ) );
-                } else {
-                    //No User Lock for the Public User Owner...
-                    localStorage.setItem("Amount_MB", 0);
-                    location.href = "Screen/OS_Loader.html";
-                }
-                return;
-            } //Do not Return Anything just add 1 to check variable
-        }
-        alert("User ID not Available");
     }
 };
 
