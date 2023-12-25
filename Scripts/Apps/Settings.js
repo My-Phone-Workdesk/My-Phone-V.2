0f789e8eb1195547b86afaef640d28cf5381e219
Vikas Sengar T0213254 <vikas.sengar@thalesgroup.com>
2023-12-25 14:11:51 +0530

    settings

---
+++ Scripts/Apps/Settings.js
@@ -0,0 +1,8 @@
+function ToggleLightorDarkMode () {
+    if ((document.body.style.backgroundColor != 'rgb(55, 55, 55)')) {
+        document.body.style.backgroundColor = '#373737'
+    } else if (document.body.style.backgroundColor = 'rgb(55, 55, 55)') {
+    document.body.style.backgroundColor = 'white'
+    }
+
+}
