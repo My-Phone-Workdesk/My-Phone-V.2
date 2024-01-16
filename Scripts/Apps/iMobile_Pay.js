16e5991090c57a3a17745b839ae43e5e9c490f89
LalaCoder <139065246+LalaCoder@users.noreply.github.com>
2024-01-16 22:14:49 +0530

    doing this + exams both 5 chapters left

--- Scripts/Apps/iMobile_Pay.js
+++ Scripts/Apps/iMobile_Pay.js
@@ -5,27 +5,36 @@ window.onload = () => {
         var option_1 = document.getElementById( 'option-1' );
         var option_2 = document.getElementById( 'option-2' );
         var option_3 = document.getElementById( 'option-3' );
+
         var modal_close_button = document.getElementById( 'closeModal' );
         var modal_switch_tab_signup = document.getElementById( 'switchTabSU' );
         var modal_switch_tab_signin = document.getElementById( 'switchTabSI' );
         var signup_button = document.getElementById( 'signupbutton' );
         var signin_button = document.getElementById( 'signinbutton' );
 
-        option_3.addEventListener( 'click', () => {
+        var modal = document.getElementById( 'SignInAndUpModal' );
 
-            return location.assign( 'Change_Details.html' );
+        modal.addEventListener( 'click', () => {
+
+            return modal.style.display = 'none';
 
         });
 
-        option_1.addEventListener( 'click', () => {
+        option_3.addEventListener( 'click', () => {
 
-            return openModal('signin');
+            return location.assign( './Change_Details.html' );
 
         });
 
         option_2.addEventListener( 'click', () => {
 
-            return openModal('signup');
+            return openModal( 'signup' );
+
+        });
+
+        option_1.addEventListener( 'click', () => {
+
+            return openModal( 'signin' );
 
         });
 
@@ -59,8 +68,6 @@ window.onload = () => {
 
         });
 
-    
-
     } else if ( location.pathname.includes( 'Change_Details.html' ) ) { 
 
         var changeDetailsButton = document.getElementById( 'changeDetails' );
@@ -80,7 +87,9 @@ window.onload = () => {
                 const inputId = checkbox.id.replace( 'Checkbox', 'Input' );
                 const input = document.getElementById( inputId );
                 input.disabled = !checkbox.checked;
-                input.className = 'change_it';
+                
+                if ( input.disabled == false ) { input.className = 'change_it'; }
+                else { input.removeAttribute( 'class' ); };
 
             });
 
@@ -106,11 +115,11 @@ function changeDetails() {
     // From AB ----> Do like this only in every script
     // But 1 line space between comments and code 😅
 
-    var name = document.getElementById( 'nameInput' ).value;
-    var username = document.getElementById( 'usernameInput' ).value;
-    var password = document.getElementById( 'passwordInput' ).value;
-    var securityCode = document.getElementById( 'securityCodeInput' ).value;
-    var mobileNumber = document.getElementById( 'mobileNumberInput' ).value;
+    var name = document.getElementById( 'nameInput' );
+    var username = document.getElementById( 'usernameInput' );
+    var password = document.getElementById( 'passwordInput' );
+    var securityCode = document.getElementById( 'securityCodeInput' );
+    var mobileNumber = document.getElementById( 'mobileNumberInput' );
     var branchCode = document.getElementById( 'branchCode' ).value;
 
     if ( branchCode != '' ) {
@@ -131,53 +140,124 @@ function changeDetails() {
         return alert( 'Authentication failed. Please enter the correct branch code.' );
 
     } else {
-    
-        // FOR AB ----> Your logic to update the account details goes here
-        // From AB ----> Thank You for that 🙏
+
+        /* FOR AB ----> Your logic to update the account details goes here
+        From AB ----> Thank You for that 🙏 */
+
+        var change_details_properties = new Object();
+
+        if ( name.className == 'change_it' ) {
+
+            change_details_properties.name = name.value;
+
+        }; if ( username.className == 'change_it' ) {
+
+            change_details_properties.username = username.value;
+
+        }; if ( password.className == 'change_it' ) {
+
+            change_details_properties.password = password.value;
+
+        }; if ( securityCode.className == 'change_it' ) {
+
+            change_details_properties.securityCode = securityCode.value;
+
+        }; if ( mobileNumber.className == 'change_it' ) {
+
+            change_details_properties.mobileNumber = mobileNumber.value;
+
+        }; const new_data = Change_for_Each_Detail( Object.keys( change_details_properties ) );
+
+        sessionStorage.setItem( 'Accounts_Data', JSON.stringify( new_data[ 0 ] ) );
+
+        var data_to_upload_on_server = new_data[ 1 ];
 
         return alert('Account details changed successfully!');
 
     };
 
+    function Change_for_Each_Detail( Detailed_list ) {
+
+        var new_data = JSON.parse( sessionStorage.getItem( 'Accounts_Data' ) );
+
+        var changing_account = new_data[ All_Branch_Codes.indexOf( branchCode ) ];
+    
+        if ( Detailed_list.indexOf( 'name' ) != -1 ) {
+         
+            changing_account.Name = change_details_properties.name;
+
+        }; if ( Detailed_list.indexOf( 'username' ) != -1 ) {
+
+            changing_account.Username = change_details_properties.username;
+
+        }; if ( Detailed_list.indexOf( 'password' ) != -1 ) {
+
+            changing_account.Password = change_details_properties.password;
+
+        }; if ( Detailed_list.indexOf( 'securityCode' ) != -1 ) {
+
+            changing_account.Security_Code = change_details_properties.securityCode;
+
+        }; if ( Detailed_list.indexOf( 'mobileNumber' ) != -1 ) {
+
+            changing_account.Mobile_Number = change_details_properties.mobileNumber;
+
+        };
+
+        return [ new_data, changing_account ];
+
+    };
+
 };
 
-function openModal(tabName) {
-    var modal = document.getElementById('SignInAndUpModal');
+function openModal( tabName ) {
+
+    var modal = document.getElementById( 'SignInAndUpModal' );
+
     modal.style.display = 'block';
 
-    document.getElementById('signin').style.display = tabName === 'signin' ? 'block' : 'none';
-    document.getElementById('signup').style.display = tabName === 'signup' ? 'block' : 'none';
-}
+    document.getElementById( 'signin' ).style.display = tabName === 'signin' ? 'block' : 'none';
+    document.getElementById( 'signup' ).style.display = tabName === 'signup' ? 'block' : 'none';
+
+    // Ohh ! The Above code is done in an advanced and 100% brain use way of javascript...
+
+};
 
 function closeModal() {
-    var modal = document.getElementById('SignInAndUpModal');
+
+    var modal = document.getElementById( 'SignInAndUpModal' );
+
     modal.style.display = 'none';
-}
 
-function switchTab(tabName) {
-    var signinTab = document.getElementById('signin');
-    var signupTab = document.getElementById('signup');
+};
 
-    if (tabName === 'signin') {
+function switchTab( tabName ) {
+
+    var signinTab = document.getElementById( 'signin' );
+    var signupTab = document.getElementById( 'signup' );
+
+    if ( tabName === 'signin' ) {
+
         signinTab.style.display = 'block';
         signupTab.style.display = 'none';
-    } else if (tabName === 'signup') {
+
+    } else if ( tabName === 'signup' ) {
+
         signinTab.style.display = 'none';
         signupTab.style.display = 'block';
-    }
-}
 
-window.onclick = function (event) {
-    var modal = document.getElementById('SignInAndUpModal');
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
+    };
+
 };
 
 function SignIn() {
-    return window.location.assign('Account.html');
-}
+
+    return window.location.assign( './Account.html' );
+
+};
 
 function SignUp() {
-    return alert('Your Branch Code is 9211');
-}
+
+    return alert( 'Your Branch Code is 9211' );
+
+};
