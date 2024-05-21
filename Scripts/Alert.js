56a7f806b4f07321621a1db1f3d03a790d976c18
LalaCoder <ayushujjwal@yahoo.com>
2024-05-21 11:59:47 +0530

    made alert.js seperate to reduce code and time

---
+++ Scripts/Alert.js
@@ -0,0 +1,42 @@
+function give_alert( statement, upcoming_function ) {
+
+    var div = document.createElement( 'div' );
+    div.id = 'alert';
+
+    var button = document.createElement( 'button' );
+    button.id = 'alert_close';
+
+    var icon = document.createElement( 'i' );
+    icon.className = 'fa-solid fa-circle-xmark';
+
+    var p = document.createElement( 'p' );
+    p.id = 'alert_statement';
+    p.innerHTML = statement;
+
+    button.appendChild( icon ); div.appendChild( button ); div.appendChild( p );
+
+    document.body.appendChild( div );
+
+    document.body.addEventListener( 'keydown', Handle_Escape_Work, true );
+
+    button.addEventListener( 'click', () => {
+        
+        div.remove(); setTimeout( () => { return upcoming_function(); },100 );
+    
+    });
+
+    function Handle_Escape_Work( event ) {
+
+        if ( event.key.toLowerCase() == 'escape' ) {
+
+            div.remove(); document.body.removeEventListener( 'keydown', Handle_Escape_Work, true );
+            
+            setTimeout( () => { return upcoming_function(); },100 );
+
+        };
+
+    };
+
+};
+
+export { give_alert };
--- Scripts/Devices.js
+++ Scripts/Devices.js
@@ -2,52 +2,9 @@
 
 import { Database } from '../Data_Resources/Database.js';
 
+import { give_alert } from './Alert.js';
+
 // Real Script Starts from Below ==>
-
-function give_alert( statement, upcoming_function ) {
-
-    var message = false;
-
-    var div = document.createElement( 'div' );
-    div.id = 'alert';
-
-    var button = document.createElement( 'button' );
-    button.id = 'alert_close';
-
-    var icon = document.createElement( 'i' );
-    icon.className = 'fa-solid fa-circle-xmark';
-
-    var p = document.createElement( 'p' );
-    p.id = 'alert_statement';
-    p.innerHTML = statement;
-
-    button.addEventListener( 'click', () => { div.remove(); return message = 'read'; });
-
-    button.appendChild( icon ); div.appendChild( button ); div.appendChild( p );
-
-    document.body.appendChild( div );
-
-    alert_read();
-
-    function alert_read() {
-
-        setTimeout( () => {
-    
-            if ( message == 'read' ) {
-    
-                return upcoming_function();
-    
-            } else {
-    
-                return alert_read();
-                
-            };
-            
-        },1000 );
-    
-    };
-
-};
 
 window.onload = () => {
 
