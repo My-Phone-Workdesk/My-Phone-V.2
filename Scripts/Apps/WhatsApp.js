835c32c9f2dee79b5d513adbc06e05e7762b6dcf
Vikas Sengar T0213254 <vikas.sengar@thalesgroup.com>
2024-06-01 13:35:10 +0530

    Please PULL

--- Scripts/Apps/WhatsApp.js
+++ Scripts/Apps/WhatsApp.js
@@ -1,1 +1,30 @@
-window.onload = () => { /* Eat 5 Star do Nothing ! */ };
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.querySelector('#theme-toggle');
+    const container = document.querySelector('.container');
+    
+    const currentTheme = sessionStorage.getItem('theme');
+    
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        container.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    toggleSwitch.addEventListener('change', () => {
+        if (toggleSwitch.checked) {
+            document.body.classList.remove('light-mode');
+            document.body.classList.add('dark-mode');
+            container.classList.remove('light-mode');
+            container.classList.add('dark-mode');
+            sessionStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+            document.body.classList.add('light-mode');
+            container.classList.remove('dark-mode');
+            container.classList.add('light-mode');
+            sessionStorage.setItem('theme', 'light-mode');
+        }
+    });    
+});
